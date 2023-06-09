2be4fd9b4e17f80990d75e2b87a986a0d3241aa2
Usami <usami@wlan-napt-006.u-aizu.ac.jp>
2023-06-09 19:23:58 +0900

    Greetings added

--- DiceGame.c
+++ DiceGame.c
@@ -7,22 +7,15 @@
 
 int main(){
 
+  char name[1000];
+  int count = 0;
+  printf("What is your name? \n");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+  
+  
 
-  int dice ;
-  int sum = 0;
-  printf("Rolling dice...\n");
-  srand((unsigned int)time(NULL));
-    
-  for(int i=1;i<=2;i++){
-    dice = MIN + rand()%RANGE;
-    printf("Die %d: %d\n",i,dice);
-    sum+=dice;
-
-  }
-
-  printf("Total value: %d\n",sum);
-
-
+ 
 
   return 0;
 }
